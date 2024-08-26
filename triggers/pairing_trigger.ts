c8ee0a05f1eebd629aae706d787da7d04b8072c9
hcoppockno10 <harry.coppock@cabinetoffice.gov.uk>
2024-08-26 14:02:38 +0100

    regular trigger schedule

---
+++ triggers/pairing_trigger.ts
@@ -0,0 +1,26 @@
+// triggers/pairing_trigger.ts
+import { Trigger } from "deno-slack-sdk/types.ts";
+import PairingWorkflow from "../workflows/pairing_workflow.ts";
+import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
+
+/**
+ * A trigger that periodically starts the pairing workflow.
+ */
+const trigger: Trigger<typeof PairingWorkflow.definition> = {
+  type: TriggerTypes.Scheduled,
+  name: "Trigger a scheduled message to encourage users to pair",
+  workflow: `#/workflows/${PairingWorkflow.definition.callback_id}`,
+  inputs: {
+    channel: {
+      value: `D069LS89Y3W`,
+    },
+  },
+  schedule: {
+    // Schedule the first execution 60 seconds from when the trigger is created
+    start_time: new Date(new Date().getTime() + 60000).toISOString(),
+    end_time: "2037-12-31T23:59:59Z",
+    frequency: { type: "hourly", repeats_every: 1 },
+  },
+};
+
+export default trigger;
